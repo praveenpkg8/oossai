4d832f91ff253bcbeaa7781c1af0bc71b02d0f83
praveen kumar <praveen.k@madstreetden.com>
2021-06-12 02:33:04 +0530

    Added: app js

---
+++ src/js/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Record from './Record';
+import ErrrorIndicator from './ErrrorIndicator';
+
+
+export default function App() {
+
+  return (
+    <>
+      <ErrrorIndicator />
+      <Record />
+    </>
+  )
+}
