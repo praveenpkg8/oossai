9034371758f33e81b3aa5e89abf59a022f9970b9
praveen kumar <praveen.k@madstreetden.com>
2021-06-20 21:05:08 +0530

    Modified: app js

--- src/js/App.js
+++ src/js/App.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import Record from './Record';
 import ErrrorIndicator from './ErrrorIndicator';
+import Setting from './Setting';
+import MouseControl from './MouseControl';
+import Help from './Help';
+
 import '../css/App.css';
 
 
@@ -9,8 +13,11 @@ export default function App() {
   return (
     <>
     <div className="container">
-      <ErrrorIndicator />
+      <MouseControl />
+      {/* <ErrrorIndicator /> */}
       <Record />
+      <Setting />
+      <Help />
     </div>
     </>
   )
