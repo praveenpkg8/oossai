41ca19f705e098ffeb50de4e67395e85418b6e54
praveen kumar <praveen.k@madstreetden.com>
2021-06-20 21:06:01 +0530

    Added: help js

---
+++ src/js/Help.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+import helpImg from '../assets/img/help.png';
+
+import '../css/help.css'
+
+
+const Help = () => {
+
+    return (
+        <>
+            <img className='help-icon' src={helpImg} alt="help" />
+        </>
+    )
+}
+
+export default Help;
