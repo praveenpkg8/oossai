1406804c8bc370b67d6a2b48d6ab6a8061cc28e4
praveen kumar <praveen.k@madstreetden.com>
2021-06-20 21:07:11 +0530

    Modified: record js

--- src/js/Record.js
+++ src/js/Record.js
@@ -1,19 +1,11 @@
 import React, { Component } from 'react';
 import { io } from "socket.io-client";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faWifi } from '@fortawesome/free-solid-svg-icons'
 
+import AudioVisualizer from './AudioVisualizer';
 
 import '../css/Record.css';
 import micOn from '../assets/img/mic_on.png';
 import micOff from '../assets/img/mic_off.png';
-import echoMicOn from '../assets/img/mic_on1.png';
-import echoMicOff from '../assets/img/mic_off1.png';
-import wifiOn from '../assets/img/wifiOn.png';
-import wifiOff from '../assets/img/wifiOff.png';
-
-
-
 
 
 import '../css/ErrrorIndicator.css';
@@ -26,7 +18,8 @@ class Record extends Component {
         this.state = {
             recording: false,
             echoRecording: false,
-            errorRecording: false
+            errorRecording: false,
+            audioVisuals: 0
         }
 
         this.socket = io(ENDPOINT);
@@ -62,6 +55,15 @@ class Record extends Component {
 
 
     }
+
+    getBufferCallback(buffers) {
+        let audioData = buffers[0].length % 7
+        this.setState({ audioVisuals: audioData })
+        console.log(audioData)
+
+    }
+
+
     startRecordingAudio = async () => {
 
         this.stream = await navigator.mediaDevices.getUserMedia({ audio: true });
@@ -72,9 +74,23 @@ class Record extends Component {
         this.rec = new Recorder(this.input, { numChannels: 1 })
         this.rec.record();
 
+        let audioVisualizerInterval = setInterval(() => {
+            const { recording } = this.state;
+            if (!recording) {
+                clearInterval(audioVisualizerInterval);
+                this.rec.clear();
+            }
+            this.rec.getBuffer(buffers => {
+                let audioData = buffers[0].length % 7
+                this.setState({ audioVisuals: audioData })
+            })
+        }, 300)
+
+
+
         let interval = setInterval(() => {
             const { recording } = this.state;
-            if(!recording) {
+            if (!recording) {
                 clearInterval(interval);
                 this.rec.clear();
             }
@@ -85,7 +101,7 @@ class Record extends Component {
                 this.socket.emit('message', { audio: blob, type: 'audio/wav' })
             })
         }, 5000)
-        
+
 
     }
     stopRecordingAudio = async () => {
@@ -93,7 +109,7 @@ class Record extends Component {
     }
 
     sendMessage = () => {
-        this.socket.emit('text', {text: "sample message"})
+        this.socket.emit('text', { text: "sample message" })
         this.socket.on('re_text', (data) => {
             console.log(data)
         })
@@ -126,13 +142,16 @@ class Record extends Component {
 
 
     render() {
-        const { recording, echoRecording, errorRecording } = this.state;
+        const { recording, audioVisuals, errorRecording } = this.state;
         return (
             <>
-                <span className={errorRecording ? 'dot-red' : 'dot-green'}></span>
-                <img className='mic' src={recording ? micOn : micOff} alt="" id="imgplus" onClick={() => {this.recordAudio(!recording)}} />
-                <img className='mic' src={echoRecording ? echoMicOn : echoMicOff} alt="" id="imgplus" onClick={this.echo} />
-                <button onClick={this.sendMessage}> Send Message</button>
+                <div className="record-wrap">
+                    <img className='mic' src={recording ? micOn : micOff} alt="" id="imgplus" onClick={() => { this.recordAudio(!recording) }} />
+                    <AudioVisualizer data={audioVisuals} />
+                    <span className={errorRecording ? 'dot-red' : 'dot-green'}></span>
+                </div>
+                {/* <img className='mic' src={echoRecording ? echoMicOn : echoMicOff} alt="" id="imgplus" onClick={this.echo} /> */}
+                {/* <button onClick={this.sendMessage}> Send Message</button> */}
             </>
         )
     }
