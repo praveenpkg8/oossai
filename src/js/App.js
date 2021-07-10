3e8b40dace71c1a371b4f8cf5d42624ae8763d2e
praveen kumar <praveen.k@madstreetden.com>
2021-07-10 12:44:34 +0530

    Commenting: mouse control

--- src/js/App.js
+++ src/js/App.js
@@ -13,8 +13,8 @@ export default function App() {
   return (
     <>
     <div className="container">
-      <MouseControl />
-      {/* <ErrrorIndicator /> */}
+      {/* <MouseControl /> */}
+      <ErrrorIndicator />
       <Record />
       <Setting />
       <Help />
