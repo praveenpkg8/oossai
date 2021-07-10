7d31b6d66fb80b66fb4aea717da33c9311535246
praveen kumar <praveen.k@madstreetden.com>
2021-07-10 12:44:57 +0530

    Modified: error indicator

--- src/js/ErrrorIndicator.js
+++ src/js/ErrrorIndicator.js
@@ -1,26 +1,30 @@
-import React, { useState } from 'react';
-import '../css/ErrrorIndicator.css';
-import wifiOn from '../assets/img/wifiOn.png';
-import wifiOff from '../assets/img/wifiOff.png';
+// App.js
+import React, { Component } from "react";
+// import "./App.css";
+import { connect, sendMsg } from "./websocket_api";
 
+class ErrorIndicator extends Component {
+  constructor(props) {
+    super(props);
+    connect();
+  }
 
-const ErrrorIndicator = () => {
-    const [internet, setInternet] = useState(window.navigator.onLine);
+  send() {
+    const _message = JSON.stringify({hello: "world"})
+    let encoder = new TextEncoder()
+    let view = encoder.encode(_message)
+    console.log(JSON.stringify({hello: "world"}));
+    console.log(view)
+    sendMsg(_message);
+  }
 
-    const checkInternet = () => {
-        setInternet(window.navigator.onLine);
-    }
-
-    setInterval(() => {
-        checkInternet();
-    }, 2000)
-
-
+  render() {
     return (
-        <>
-            <img className='wifi' src={internet ? wifiOn : wifiOff} alt="" />
-        </>
-    )
+      <div className="App">
+        <button onClick={this.send}>Hit</button>
+      </div>
+    );
+  }
 }
 
-export default ErrrorIndicator;
+export default ErrorIndicator;
