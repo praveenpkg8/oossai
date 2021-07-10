d9b1b0033b0c7919093e69c8ce933c27b819637a
praveen kumar <praveen.k@madstreetden.com>
2021-07-10 12:39:22 +0530

    Changed: main js

--- main.js
+++ main.js
@@ -1,17 +1,41 @@
 const { BrowserWindow, app, ipcMain, Notification } = require('electron');
-// const python = require('child_process').spawn('python', ['./hello.py']);
+const _path = app.getAppPath()
+
+const python = require('child_process').spawn('python', [_path+'/hello.py']);
 // const {exec} = require('child_process');
 // exec("sh ./hello.py", () => {})
    
 const path = require('path');
+const { fs } = require('fs')
+
+async function exists (path) {  
+  try {
+    console.log(path)
+    await console.log(Fs.access(path))
+    return true
+  } catch {
+    return false
+  }
+}
+
+console.log('Your App Path:' + _path)
+
 
 const isDev = !app.isPackaged;
+const height = 70
+const width = 500
 
 
 function createWindow() {
   const win = new BrowserWindow({
-    width: 500,
-    height: 120,
+    width: width,
+    height: height,
+    minWidth: width,
+    maxWidth: width,
+    minHeight: height,
+    maxHeight: height,
+    transparent: true, 
+    frame: false,
     backgroundColor: "#424141",
     webPreferences: {
       nodeIntegration: true,
@@ -33,19 +57,33 @@ function createWindow() {
 ipcMain.on('asynchronous-message', (event, arg) => {
   console.log(arg)
 
-  const testWin = new BrowserWindow({
-    width: 1200,
-    height: 800,
-    backgroundColor: "#424141",
-    webPreferences: {
-      nodeIntegration: true,
-      worldSafeExecuteJavaScript: true,
-      contextIsolation: false,
-      // preload: path.join(__dirname, 'preload.js')
-    }
-  })
+  if (fs.existsSync(_path+'/hello.py')) {
+    console.log('exists')
+  } else {
+    console.log('does not')
+  }
 
-  testWin.loadFile(path.join(__dirname, 'index.html'));
+
+
+  
+  event.sender.send('rec_reply', "async pong")
+
+  // const testWin = new BrowserWindow({
+  //   width: 1200,
+  //   height: 800,
+  //   transparent: true, 
+  //   frame: false,
+  //   // backgroundColor: "#424141",
+  //   backgroundColor: "white",
+  //   webPreferences: {
+  //     nodeIntegration: true,
+  //     worldSafeExecuteJavaScript: true,
+  //     contextIsolation: false,
+  //     // preload: path.join(__dirname, 'preload.js')
+  //   }
+  // })
+
+  // testWin.loadFile(path.join(__dirname, 'index.html'));
 
   // Event emitter for sending asynchronous messages
   // event.sender.send('asynchronous-reply', 'async pong')
