5b1a39ce857574aadba54d50f72920b591c4eaa9
praveen kumar <praveen.k@madstreetden.com>
2021-07-17 16:45:47 +0530

    Modified: audio visualizer

--- src/js/AudioVisualizer.js
+++ src/js/AudioVisualizer.js
@@ -1,17 +1,31 @@
 import React, { useState } from 'react';
-import '../css/AudioVisualizer.css'
+import '../css/AudioVisualizer.css';
+
+
+const makeid = (length) => {
+    let result = '';
+    let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let charactersLength = characters.length;
+    for (var i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() *
+            charactersLength));
+    }
+    return result;
+}
+
 
 const AudioVisualizer = (props) => {
-    let visualizer = []
-    // let visual = Math.floor((Math.random() * 10) % 7)
-    const {data} = props
-    console.log(data)
-    let visual = data
+
+    let visualizer = [];
+    const { data } = props;
+    let visual = data;
+
     for (let i = 0; i < 7; i++) {
-        visualizer.push(i > visual ? "visualizer" : "visualizer-trans")
-    }
+        visualizer.push(i > visual ? "visualizer" : "visualizer-trans");
+    };
+
     const visualItem = visualizer.map((object, index) => (
-        <span className={`${object} visualizer-${index}`}></span>
+        <span key={makeid(16)} className={`${object} visualizer-${index}`}></span>
     ));
 
     return (
