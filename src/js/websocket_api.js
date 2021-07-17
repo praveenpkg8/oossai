faec44cd7042dd43713da0cd7912718af262a3d8
praveen kumar <praveen.k@madstreetden.com>
2021-07-17 16:44:45 +0530

    Modified: websocket api

--- src/js/websocket_api.js
+++ src/js/websocket_api.js
@@ -1,29 +1,40 @@
-// api/index.js
-var socket = new WebSocket("ws://localhost:8080/ws");
+const socket = new WebSocket("ws://localhost:8080/ws");
+socket.binaryType = "arraybuffer";
+let errorConnection = true;
 
 let connect = () => {
   console.log("Attempting Connection...");
 
   socket.onopen = () => {
     console.log("Successfully Connected");
+    errorConnection = false;
   };
 
   socket.onmessage = msg => {
     console.log(msg);
+    console.log(typeof(msg))
   };
 
   socket.onclose = event => {
     console.log("Socket Closed Connection: ", event);
+    errorConnection = true;
   };
 
   socket.onerror = error => {
     console.log("Socket Error: ", error);
+    errorConnection = true;
   };
+  return errorConnection
 };
 
-let sendMsg = msg => {
-  console.log("sending msg: ", typeof(msg));
-  socket.send(msg);
+let getConnectionStatus = () => {
+  return errorConnection
+}
+
+let sendMsg = data => {
+  console.log("sending msg type: ", typeof(data));
+  console.log("sending msg: ", data);
+  socket.send(data);
 };
 
-export { connect, sendMsg };
+export { connect, sendMsg, getConnectionStatus };
